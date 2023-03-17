728e626c6c9b03feb46f04809275764708b01c72
Michel Morais <valentimmichel@gmail.com>
2023-03-17 09:51:36 -0300

    Exception call in main program

--- src/Generics.java
+++ src/Generics.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 import services.PrintService;
 
 public class Generics {
-	//Solution with generics
+	// Solution with generics
+	//Exception call in main program
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		PrintService<Integer> ps = new PrintService<>();
-		
-		System.out.print("How many values? ");
-		int n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			Integer value = sc.nextInt();
-			ps.addValue(value);
+
+		try {
+			PrintService<Integer> ps = new PrintService<>();
+
+			System.out.print("How many values? ");
+			int n = sc.nextInt();
+
+			for (int i = 0; i < n; i++) {
+				Integer value = sc.nextInt();
+				ps.addValue(value);
+			}
+
+			ps.print();
+			Integer x = ps.first();
+			System.out.println("First: " + x);
+
+		} catch (IllegalStateException e) {
+			System.out.println(e.getMessage());
 		}
-		
-		ps.print();
-		Integer x = ps.first();
-		System.out.println("First: " + x);
-		
+
 		sc.close();
 	}
 
